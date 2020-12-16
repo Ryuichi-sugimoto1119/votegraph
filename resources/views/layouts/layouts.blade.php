fdccde3a3d4aa5eee206c5735d6d0f5259d73ee5
EC2 Default User <ec2-user@ip-172-31-11-83.ap-northeast-1.compute.internal>
2020-12-16 06:24:41 +0000

    deploy to heroku pre

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -30,6 +30,6 @@
         @component('components.footer')
         @endcomponent
         
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html> 
